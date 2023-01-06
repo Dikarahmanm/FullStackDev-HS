e1acb89abc98df8a83e6926271cbbe90d3cf369e
Dikarahmanm <dhikarm78@gmail.com>
2023-01-06 14:01:15 +0700

    catalog

---
+++ Project/Katalog/tailwind.config.js
@@ -0,0 +1,18 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        olive: "#F6F2E4",
+        ingreen: "#6E7E2B",
+        cotext: "#7D794F",
+      },
+    },
+  },
+  plugins: [],
+};
